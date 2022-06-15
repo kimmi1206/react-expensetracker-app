f063494f9998426cf5c2a7cb4955a0b611b9cf56
kimmi1206 <kheinerimm1206@gmail.com>
2022-06-15 15:55:27 -0500

    style component props

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,7 +8,7 @@ const Button = styled.button`
   background: transparent;
   border-radius: 3px;
   border: 2px solid palevioletred;
-  color: palevioletred;
+  color: ${(props) => props.inputColor || 'palevioletred'};
   margin: 0.5em 1em;
   padding: 0.25em 1em;
 
