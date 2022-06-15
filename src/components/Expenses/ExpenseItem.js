ceee2ed865642d5587018ea5c7609b1597c5637f
kimmi1206 <kheinerimm1206@gmail.com>
2022-06-15 15:42:31 -0500

    Styled Components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,27 @@ import React from 'react';
 import './ExpenseItem.css';
 // import styles from './ExpenseItem.module.css';
 import ExpenseDate from './ExpenseDate';
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`;
 
 // Class based components require more code, it's recommended react hooks useState
 export default class ExpenseItem extends React.Component {
@@ -30,6 +51,10 @@ export default class ExpenseItem extends React.Component {
         <ExpenseDate date={this.state.date} />
         <div className='expense-item__description'>
           <h2>{this.state.title}</h2>
+          <Container>
+            <Button>Normal Button</Button>
+            <Button primary>Primary Button</Button>
+          </Container>
           <div className='expense-item__price'>{this.state.amount}</div>
         </div>
       </div>
